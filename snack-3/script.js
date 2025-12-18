9c90d3665bfe3e23cd1344e55cc4a1a235dcde1a
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2025-12-18 16:33:51 +0100

    Snak 3 e 4 completati

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const arrayPlusOne = [];
+
+numbers.forEach ((number, index, numbers) => {
+    arrayPlusOne.push(`Numero incrementato di una unità: ${number + 1}`);
+});
+
+
+    console.log(arrayPlusOne);
